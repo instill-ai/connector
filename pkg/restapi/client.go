f3648b752907d8a5be7858c47005f11f5cf6e729
Juan Vallés <jvallesm@gmail.com>
2024-01-04 16:38:14 +0100

    feat: extract http client from REST API to httpclient

--- pkg/restapi/client.go
+++ pkg/restapi/client.go
@@ -1,20 +1,9 @@
 package restapi
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
-	"net/url"
-	"time"
-
 	"github.com/instill-ai/connector/pkg/util/httpclient"
+	"go.uber.org/zap"
 	"google.golang.org/protobuf/types/known/structpb"
-)
-
-const (
-	reqTimeout = time.Second * 60 * 5
 )
 
 type TaskInput struct {
@@ -28,122 +17,19 @@ type TaskOutput struct {
 	Header     map[string][]string    `json:"header"`
 }
 
-type Client struct {
-	BaseURL        string         `json:"base_url"`
-	Authentication Authentication `json:"authentication"`
-	HTTPClient     httpclient.Doer
-}
+func newClient(config *structpb.Struct, logger *zap.Logger) (*httpclient.Client, error) {
+	c := httpclient.New("REST API", getBaseURL(config),
+		httpclient.WithLogger(logger),
+	)
 
-func NewClient(config *structpb.Struct) (Client, error) {
-	baseURL := getBaseURL(config)
 	auth, err := getAuthentication(config)
 	if err != nil {
-		return Client{}, err
+		return nil, err
 	}
 
-	tr := &http.Transport{
-		DisableKeepAlives: true,
+	if err := auth.setAuthInClient(c); err != nil {
+		return nil, err
 	}
 
-	return Client{
-		BaseURL:        baseURL,
-		Authentication: auth,
-		HTTPClient: &http.Client{
-			Timeout:   reqTimeout,
-			Transport: tr,
-		},
-	}, nil
-}
-
-func (c *Client) sendRequest(method string, input TaskInput) (TaskOutput, error) {
-	resp := TaskOutput{}
-
-	http.DefaultClient.Timeout = reqTimeout
-
-	reqURL := c.BaseURL
-	if input.EndpointPath != nil {
-		reqURL += *input.EndpointPath
-	}
-
-	var req *http.Request
-	var err error
-
-	switch method {
-	case http.MethodGet, http.MethodHead:
-		// Ignore body
-		req, err = http.NewRequest(method, reqURL, nil)
-		if err != nil {
-			return resp, err
-		}
-	case http.MethodPost, http.MethodPut, http.MethodPatch, http.MethodDelete:
-		if input.Body == nil {
-			req, err = http.NewRequest(method, reqURL, nil)
-			if err != nil {
-				return resp, err
-			}
-		} else {
-			// Convert body to JSON
-			jsonBody, err := json.Marshal(input.Body)
-			if err != nil {
-				return resp, err
-			}
-			req, err = http.NewRequest(method, reqURL, bytes.NewBuffer(jsonBody))
-			if err != nil {
-				return resp, err
-			}
-		}
-	default:
-		return resp, fmt.Errorf("not supported method: %s", method)
-	}
-
-	// Add authentication
-	switch c.Authentication.GetAuthLocation() {
-	case Header:
-		key, value, err := c.Authentication.GenAuthHeader()
-		if err != nil {
-			return resp, err
-		}
-		if key != "" {
-			req.Header.Add(key, value)
-		}
-	case Query:
-		key, value, err := c.Authentication.GenAuthQuery()
-		if err != nil {
-			return resp, err
-		}
-		if key != "" {
-			// Append the query parameter to the URL
-			extraParams := url.Values{}
-			extraParams.Add(key, value)
-			if req.URL.RawQuery != "" {
-				req.URL.RawQuery += "&" + extraParams.Encode()
-			} else {
-				req.URL.RawQuery = extraParams.Encode()
-			}
-		}
-	default:
-		return resp, nil
-	}
-
-	// Send request
-	res, err := c.HTTPClient.Do(req)
-	if res != nil && res.Body != nil {
-		defer res.Body.Close()
-	}
-	if err != nil {
-		return resp, err
-	}
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		return resp, err
-	}
-
-	resp.StatusCode = res.StatusCode
-	resp.Header = res.Header
-	err = json.Unmarshal(body, &resp.Body)
-	if err != nil {
-		return resp, err
-	}
-	return resp, nil
+	return c, nil
 }
--- pkg/restapi/main.go
+++ pkg/restapi/main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/gofrs/uuid"
 	"github.com/instill-ai/component/pkg/base"
+	"github.com/instill-ai/x/errmsg"
 	"go.uber.org/zap"
 	"google.golang.org/protobuf/types/known/structpb"
 
@@ -25,14 +26,26 @@ const (
 	taskOptions = "TASK_OPTIONS"
 )
 
-//go:embed config/definitions.json
-var definitionsJSON []byte
+var (
+	//go:embed config/definitions.json
+	definitionsJSON []byte
 
-//go:embed config/tasks.json
-var tasksJSON []byte
+	//go:embed config/tasks.json
+	tasksJSON []byte
 
-var once sync.Once
-var connector base.IConnector
+	once      sync.Once
+	connector base.IConnector
+
+	taskMethod = map[string]string{
+		taskGet:     http.MethodGet,
+		taskPost:    http.MethodPost,
+		taskPatch:   http.MethodPatch,
+		taskPut:     http.MethodPut,
+		taskDelete:  http.MethodDelete,
+		taskHead:    http.MethodHead,
+		taskOptions: http.MethodOptions,
+	}
+)
 
 type Connector struct {
 	base.Connector
@@ -67,34 +80,34 @@ func getBaseURL(config *structpb.Struct) string {
 	return config.GetFields()["base_url"].GetStringValue()
 }
 
-func getAuthentication(config *structpb.Struct) (Authentication, error) {
+func getAuthentication(config *structpb.Struct) (authentication, error) {
 	auth := config.GetFields()["authentication"].GetStructValue()
 	authType := auth.GetFields()["auth_type"].GetStringValue()
 
 	switch authType {
-	case string(NoAuthType):
-		authStruct := NoAuth{}
+	case string(noAuthType):
+		authStruct := noAuth{}
 		err := base.ConvertFromStructpb(auth, &authStruct)
 		if err != nil {
 			return nil, err
 		}
 		return authStruct, nil
-	case string(BasicAuthType):
-		authStruct := BasicAuth{}
+	case string(basicAuthType):
+		authStruct := basicAuth{}
 		err := base.ConvertFromStructpb(auth, &authStruct)
 		if err != nil {
 			return nil, err
 		}
 		return authStruct, nil
-	case string(APIKeyType):
-		authStruct := APIKeyAuth{}
+	case string(apiKeyType):
+		authStruct := apiKeyAuth{}
 		err := base.ConvertFromStructpb(auth, &authStruct)
 		if err != nil {
 			return nil, err
 		}
 		return authStruct, nil
-	case string(BearerTokenType):
-		authStruct := BearerTokenAuth{}
+	case string(bearerTokenType):
+		authStruct := bearerTokenAuth{}
 		err := base.ConvertFromStructpb(auth, &authStruct)
 		if err != nil {
 			return nil, err
@@ -106,61 +119,66 @@ func getAuthentication(config *structpb.Struct) (Authentication, error) {
 }
 
 func (e *Execution) Execute(inputs []*structpb.Struct) ([]*structpb.Struct, error) {
-
-	client, err := NewClient(e.Config)
+	client, err := newClient(e.Config, e.Logger)
 	if err != nil {
 		return nil, err
 	}
 
+	method, ok := taskMethod[e.Task]
+	if !ok {
+		return nil, errmsg.AddMessage(
+			fmt.Errorf("not supported task: %s", e.Task),
+			fmt.Sprintf("%s task is not supported.", e.Task),
+		)
+	}
+
 	outputs := []*structpb.Struct{}
 	for _, input := range inputs {
-		var method string
-		inputStruct := TaskInput{}
-		switch e.Task {
-		case taskGet:
-			method = http.MethodGet
-		case taskPost:
-			method = http.MethodPost
-		case taskPatch:
-			method = http.MethodPatch
-		case taskPut:
-			method = http.MethodPut
-		case taskDelete:
-			method = http.MethodDelete
-		case taskHead:
-			method = http.MethodHead
-		case taskOptions:
-			method = http.MethodOptions
-		default:
-			return nil, fmt.Errorf("not supported task: %s", e.Task)
+		taskIn := TaskInput{}
+		taskOut := TaskOutput{}
+		path := ""
+
+		if err := base.ConvertFromStructpb(input, &taskIn); err != nil {
+			return nil, err
 		}
 
-		err := base.ConvertFromStructpb(input, &inputStruct)
+		if taskIn.EndpointPath != nil {
+			path = *taskIn.EndpointPath
+		}
+
+		// An API error is a valid output in this connector.
+		req := client.R().SetResult(&taskOut.Body).SetError(&taskOut.Body)
+		if taskIn.Body != nil {
+			req.SetBody(taskIn.Body)
+		}
+
+		resp, err := req.Execute(method, path)
 		if err != nil {
 			return nil, err
 		}
-		outputStruct, err := client.sendRequest(method, inputStruct)
+
+		taskOut.StatusCode = resp.StatusCode()
+		taskOut.Header = resp.Header()
+
+		output, err := base.ConvertToStructpb(taskOut)
 		if err != nil {
 			return nil, err
 		}
-		output, err := base.ConvertToStructpb(outputStruct)
-		if err != nil {
-			return nil, err
-		}
+
 		outputs = append(outputs, output)
 	}
 	return outputs, nil
 }
 
 func (c *Connector) Test(defUid uuid.UUID, config *structpb.Struct, logger *zap.Logger) (pipelinePB.Connector_State, error) {
-	client, err := NewClient(config)
+	client, err := newClient(config, logger)
 	if err != nil {
 		return pipelinePB.Connector_STATE_ERROR, err
 	}
-	_, err = client.sendRequest(http.MethodGet, TaskInput{})
-	if err != nil {
-		return pipelinePB.Connector_STATE_ERROR, err
-	}
-	return pipelinePB.Connector_STATE_CONNECTED, nil
 
+	if _, err := client.R().Get(""); err != nil {
+		return pipelinePB.Connector_STATE_ERROR, err
+	}
+
+	return pipelinePB.Connector_STATE_CONNECTED, nil
 }
