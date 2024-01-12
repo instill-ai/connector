dabfc199fa3f45f8ddb33d58146bf756a656ba61
Chang, Hui-Tang <huitang.chang@instill.tech>
2024-01-12 11:33:34 +0800

    feat(instill): generate enumeration for `model_name` automatically (#100)
    Because
    
    - Originally, we used `model_namespace` and `model_id` to set up the
    Model connector. However, users may struggle to identify the correct
    values for these settings. To enhance user-friendliness, we should
    employ enumerations (options).
    
    This commit
    
    - generate enumeration for `model_name` automatically

--- pkg/instill/client.go
+++ pkg/instill/client.go
@@ -8,6 +8,7 @@ import (
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
 
+	mgmtPB "github.com/instill-ai/protogen-go/core/mgmt/v1beta"
 	modelPB "github.com/instill-ai/protogen-go/model/model/v1alpha"
 )
 
@@ -28,6 +29,24 @@ func initModelPublicServiceClient(serverURL string) (modelPB.ModelPublicServiceC
 	}
 
 	return modelPB.NewModelPublicServiceClient(clientConn), clientConn
+}
+
+func initMgmtPublicServiceClient(serverURL string) (mgmtPB.MgmtPublicServiceClient, *grpc.ClientConn) {
+	var clientDialOpts grpc.DialOption
+
+	if strings.HasPrefix(serverURL, "https://") {
+		clientDialOpts = grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{InsecureSkipVerify: true}))
+	} else {
+		clientDialOpts = grpc.WithTransportCredentials(insecure.NewCredentials())
+	}
+
+	serverURL = stripProtocolFromURL(serverURL)
+	clientConn, err := grpc.Dial(serverURL, clientDialOpts)
+	if err != nil {
+		return nil, nil
+	}
+
+	return mgmtPB.NewMgmtPublicServiceClient(clientConn), clientConn
 }
 
 func stripProtocolFromURL(url string) string {
